d0237a909ae123d20b11b954fb8f1e583549cfd8
Ayush Kacholiya <77440402+Ayush8757@users.noreply.github.com>
2022-12-09 22:42:11 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const hamBtn = document.getElementById("hamburgerBtn"),
+      hamView = document.getElementById("hamburgerView"),
+      bodyView = document.getElementsByTagName("BODY")[0];
+
+
+      function menu() {
+        hamView.classList.toggle("open");
+        hamView.classList.toggle("hidden");
+        bodyView.classList.toggle("menuOpen");
+    }
+    
+    function removeMenu() {
+        hamView.classList.remove("open");
+        hamView.classList.add("hidden");
+        bodyView.classList.remove("menuOpen");
+    }
+
+
+
+$(function() {
+
+	if ( $('.owl-2').length > 0 ) {
+        $('.owl-2').owlCarousel({
+            center: false,
+            items: 1,
+            loop: true,
+            stagePadding: 0,
+            margin: 20,
+            smartSpeed: 1000,
+            autoplay: true,
+            nav: true,
+            dots: true,
+            pauseOnHover: false,
+            responsive:{
+                600:{
+                    margin: 20,
+                    nav: true,
+                  items: 2
+                },
+                1000:{
+                    margin: 20,
+                    stagePadding: 0,
+                    nav: true,
+                  items: 3
+                }
+            }
+        });            
+    }
+
+})
+
